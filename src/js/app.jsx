a1d4142a00fbef0e934cd694408d53e53c97c0ec
Nick Terwoord <nick@triggit.com>
2015-03-28 14:15:11 -0400

    Initial Structure
    Defines enough initial structure to be able to build.
    
    Can worry about `watch`, production environments, `css` and etc. later.

---
+++ src/js/app.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+window.React = React;
+
+window.MyClass = React.createClass({
+    render: function () {
+        return (
+            <div id="container"></div>
+        );
+    }
+});
